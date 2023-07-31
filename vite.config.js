90f17c75db542cdc3e799ba767555bcc3451a60e
Ujala Khurram <ujalakhurram187@gmail.com>
2023-07-31 19:27:06 +0500

    removed gh-pages package

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/campbuddy/",
   plugins: [svgr(), react()],
   resolve: {
     alias: {
@@ -13,4 +14,4 @@ export default defineConfig({
       layout: "/src/layout",
     },
   },
-})
+});
