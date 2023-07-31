08ec99d69eb5f5ded7151cf4b15c8ccdca970b81
Ujala Khurram <ujalakhurram187@gmail.com>
2023-07-31 19:28:11 +0500

    heading and body font styles set to global

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -6,11 +6,9 @@ const Hero = () => {
     <section className="bg-hero bg-fixed bg-left-bottom bg-no-repeat bg-cover">
       <Header />
       <div className="py-52 sm:py-60 xl:py-72 px-6 text-center text-white 2xl:container">
-        <h1 className="xl:text-6xl sm:text-5xl text-4xl mb-6 font-serif font-bold">
-          Discover the best Campgrounds
-        </h1>
-        <p className="mb-6 mx-auto font-sans sm:w-3/5">
-          Find, Add and review your favourite campgrounds in USA National Parks
+        <h1 className="mb-6">Discover the best Campgrounds</h1>
+        <p className="mb-6 mx-auto sm:w-3/5">
+          Find and review your favourite campgrounds in USA National Parks
         </p>
         <Button url="#" classes="inline-block w-11/12 sm:max-w-fit">
           Start Exploring
