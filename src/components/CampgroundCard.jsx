1cf9e3de24a540bd90c986bf032163d14c6dbb25
Ujala Khurram <ujalakhurram187@gmail.com>
2023-08-01 19:47:40 +0500

    campgrounds card added

---
+++ src/components/CampgroundCard.jsx
@@ -0,0 +1,22 @@
+import ImageCircle from "components/ImageCircle";
+
+const CampgroundCard = (props) => {
+  const { image, campgroundName, location } = props;
+
+  return (
+    <div className="w-full overflow-hidden rounded-xl border border-solid border-light-green bg-grayish-white p-5 sm:max-w-sm">
+      <ImageCircle
+        image={image}
+        classes={{
+          container: "-mr-16 -mt-16 w-11/12",
+          img: "",
+          brownCircle: "bottom-[20%]",
+        }}
+      />
+      <h5 className="mt-8">{campgroundName}</h5>
+      <p className="text-gold-green">{location}</p>
+    </div>
+  );
+};
+
+export default CampgroundCard;
